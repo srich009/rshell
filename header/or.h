22f312c91730ea149d3dad7cc2b9df5aef016cd8
srich009 <srich009@ucr.edu>
2016-10-20 22:22:13 +0000

    resolve conflict in or.h

--- header/or.h
+++ header/or.h
@@ -6,14 +6,10 @@
 
 class Or : public Connector
 {
-<<<<<<< HEAD
-
-=======
     protected:
     
         std::vector<std::string> v;
     
->>>>>>> 62098a3c7ab8a116ab0b3e5969fcdfea1522fe94
     public:
     
         Or(std::vector<std::string> vs);
