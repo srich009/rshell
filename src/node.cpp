75b9fb27f10f109a349020f9bf57d6565f3eb385
srich009 <srich009@ucr.edu>
2016-10-20 04:52:13 +0000

    node.cpp implemented

---
+++ src/node.cpp
@@ -0,0 +1,34 @@
+// Node class implementation
+
+#include "node.h"
+
+
+Node::Node(std::string s)
+    : key(s), parent(0), left(0), right(0)
+{}
+//-----------------------------------------------    
+    
+Node* Node::getPar()
+{
+    return parent;
+}
+//-----------------------------------------------
+
+Node* Node::getLeft()
+{
+    return left;
+}
+//-----------------------------------------------
+
+Node* Node::getRight()
+{
+    return right;
+}
+//-----------------------------------------------
+
+std::string Node::getKey()
+{
+    return key;
+}
+//-----------------------------------------------
+
