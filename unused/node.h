0ab21999a8d9bd89dcc1a7b77875f05f750d571f
srich009 <srich009@ucr.edu>
2016-10-28 04:02:25 +0000

    remove node because related to maketree

---
+++ unused/node.h
@@ -0,0 +1,44 @@
+#ifndef __NODE_H__
+#define __NODE_H__
+
+#include <string>
+
+class Node
+{
+
+    private:
+    
+        // represents either Command or Connerctor
+        std::string key;
+        
+        Node* parent;
+        
+        Node* left;
+        
+        Node* right;
+
+    public:
+
+    	Node(std::string);
+    	
+    	~Node();
+    
+        void clear(Node* n);
+
+    	void setPar(Node* n);
+    	
+    	void setLeft(Node* n);
+    	
+    	void setRight(Node* n);
+    	
+    	Node* getPar();
+    	
+    	Node* getLeft();
+    	
+    	Node* getRight();
+    	
+    	std::string getKey();
+
+};
+
+#endif // __NODE_H__
