0770b354fa51ca6e4e760f894b7e34cf8252312a
srich009 <srich009@ucr.edu>
2016-11-13 21:18:26 +0000

    trimming

--- src/action.cpp
+++ src/action.cpp
@@ -59,16 +59,16 @@ bool Action::exec(Node* n) // tree traversal algorithm
         else // recursive solve  executr function part B
         {            
             if(str == "&&" && c == true) // &&
-            {                std::cout << "str: " << str << std::endl;
+            {                
                 b = exec(n->getRight());
             }
             else if(str == "||" && c == false) // ||
-            {                std::cout << "str: " << str << std::endl;
+            {               
                 b = exec(n->getRight());
             }
             else if(str == ";") // ;
             {  
-                std::cout << "str: " << str << std::endl;
+                if(n->getRight() == 0){return c;} // ?? if is "cmd;"  where semi used as terminator
                 b = exec(n->getRight());
             }
             else
@@ -77,6 +77,8 @@ bool Action::exec(Node* n) // tree traversal algorithm
                 // perror("ERROR: Unknown connector");
                 // exit(1);
             }
+            
+            return b;
         }
 
     }
@@ -144,89 +146,3 @@ int Action::executr(char* cmd) // execute char[] with execvp syscalls
     return 1;
 }
 //-----------------------------------------------
-
-
-
-
-
-
-/// OLD VERSION OF EXEC
-
-    // bool b = true;
-    
-    // for(unsigned i = 0; i < o.size(); i++)
-    // {
-    //     if(o.at(i)->type() == "Command")      //if its a command execute it
-    //     {            
-    //         std::string com = o.at(i)->get(); //gets string literal
-    
-    //         if(com == "exit ")
-    //         {
-    //             exit(0); 
-    //         }
-    //         // else if(com == "cd ") // NOT USED
-    //         // {
-    //         //     b = cd(args);
-    //         // }
-    //         else
-    //         {
-    //             //bin
-    //             std::string in = o.at(i)->get();
-    //             const char* in1 = in.c_str();
-    //             int flag = executr((char*)(in1));
-    //             if(flag == 1)
-    //             {
-    //                 b = true;
-    //             }
-    //             else
-    //             {
-    //                 b = false;
-    //             }
-    //         }
-    //     }
-    //     else //if it is a connector
-    //     {            
-    //         if(o.at(i)->type() == "And" && b == false)
-    //         {
-    //             i++;
-    //         }
-    //         else if(o.at(i)->type() == "Or" && b == true)
-    //         {
-    //             i++;
-    //         }
-    //         else
-    //         {
-    //             //do nothing if semicolon
-    //         }
-    //     }
-    // }
-//------------------------------------------------------------------
-    
-    // psuedo code ... 
-    /*
-    
-        let T be root node tree;
-        
-        if(T != NULL)
-        {
-            return t.value;
-        }
-        
-        // recursive solve   // solve is executr function
-        A = solve.(t->left);
-        B = solve(t->right);
-        
-        // calculate and apply the operator "T.value" to A and B, return result/error
-        try
-        {
-            calculate(A, B, T.value); // calculate handles logic control flow {"&&", "||", ";"}
-            return;
-        }
-        catch(std::exception &e)
-        {
-            cout << e.what() << endl;
-            exit(1);
-        }
-        
-        
-    */
